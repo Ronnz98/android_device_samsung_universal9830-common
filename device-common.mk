2e65466daf5a6506e801e397946bd680034e7d17
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2026-06-26 11:05:02 +0200

    Update device-common.mk
    PhhIms - VoLTE/VoWiFi for LineageOS on Samsung Exynos devices from
    https://github.com/krazey/ims

--- device-common.mk
+++ device-common.mk
@@ -145,6 +145,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.allow_ims_off=1
 
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/privapp-permissions-me.phh.ims.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-me.phh.ims.xml
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.exynos990 \
